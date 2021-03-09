534b82c469d815df66e733b017cfce8fb9d8a94f
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-09 18:07:33 -0300

    Documentation

--- mix.exs
+++ mix.exs
@@ -8,6 +8,16 @@ defmodule ListLength.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+
+      #Docs
+      name: "ListLength",
+      source_url: "https://github.com/svgreve/ListLength",
+      homepage_url: "http://localhost:4000/ListLength",
+      docs: [
+        main: "ListLength",
+        logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +33,7 @@ defmodule ListLength.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
     ]
   end
 end
