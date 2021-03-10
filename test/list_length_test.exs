d0b2e50a5b3c7cb5deb37db0ccd348990b7812ae
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-10 12:07:03 -0300

    Tests

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -2,7 +2,21 @@ defmodule ListLengthTest do
   use ExUnit.Case
   doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+
+    test "returns number of list numeric elements" do
+      list = [ 1, 2, 3, 4 ]
+      response = ListLength.call(list)
+      expected_response = 4
+      assert response == expected_response
+    end
+
+    test "returns number of list generic elements" do
+      list = [ "first element", :atom, 3.0, <<10>> ]
+      response = ListLength.call(list)
+      expected_response = 4
+      assert response == expected_response
+    end
+
   end
 end
